5d4fa9f61ed0edafdbec7b7fc11a08b96a896f83
kousu <nick@kousu.ca>
2013-11-27 22:24:07 -0500

    Circumvent broken ogr::SpatialFilter with a filter loop. Curiously, I can use almost entirely gdal calls to do this, just not rely on SetSpatialFilterRect to properly filter anything

--- scratch/geojson.py
+++ scratch/geojson.py
@@ -6,6 +6,10 @@
 
 import os
 import osgeo.ogr as ogr
+ogr.UseExceptions() #make ogr closer to sane
+assert ogr.GetUseExceptions() == True
+
+
 #import geojson
 
 
@@ -21,7 +25,127 @@ import IPython
 IPython.terminal.embed.TerminalInteractiveShell.confirm_exit=False #i want
 #IPython.get_config().InteractiveShell.confirm_exit = False
 
+def features(shapefile):
+	"a simple iterator that returns items from a shapefile"
+	"precondition: your shapefile has exactly one layer (you can split it up with gdal or qgis if this is not true)"
+	input = ogr.Open(shapefile)
+	assert input.GetLayerCount() == 1, "This simple iterator only supports single-layer files"
+	
+	layer_in = input.GetLayerByIndex(0);
+	return (layer_in.GetFeature(f) for f in range(layer_in.GetFeatureCount()))
+
+
+
+
+def filterrect(polygons, left, right, bottom, top):
+	"ogr::Layer::SetSpatialFilterRect seems to be buggy, or I'm misunderstand the docs"
+	"this hack implements it plus the iteration to actually extract filtered features"
+	"note that the order of arguments is flipped to my intuition (and what ogr::Geometry::GetEnvelope() returns...) says they should have"
+	
+	if isinstance(polygons, str):
+		polygons = featues(polygons)  #I *believe* that this should (even if polygons happened to already be opened)
+	
+	def p_inrect(point):
+		"return true if the point ... is"
+		raise NotImplemented
+	
+	def inrect(geometry):
+		"a 'geometry' is a point, line, or polygon, or compositions thereof"
+		"i.e. something on the map"
+		assert geometry.GetGeometryType() == ogr.wkbPolygon, "TODO: support more than just polygons"
+		
+		
+	# extract the geometry object that gdal creates when we call it
+	layer.SetSpatialFilterRect(left, bottom, right, top)  
+	clip = layer.GetSpatialFilter() 
+	layer.SetSpatialFilter(None)   #unset the filter since we're doing it manually
+	def gen():
+		for f in polygons:
+			if f.intersects(clip):
+				yield f
+	#return layer.
+
+
+#todo: write generic filtering thing
+
+def filter_to_elora_really():
+	driverName = "ESRI Shapefile"
+	
+	# near Elora, Ontario, which is
+	# #  -80.42722 , 43.685
+	# the lat/lon system is deformed so that
+	# # a unit latitude is 8.3km, and a unit longitude is 10km (<-- is this right??)
+	# for ourpuroses, taking a couple of degrees in each direction should be good ... 79 to 81 
+	elora = ((-81, 42), (-79, 44)) #(bottomleft, topright), to be in line with SetSpatialFilterRect()
+
+	shapefile = "../assets/maps/OGDE_ARI/AgricultureResourceInventory_1983"
+	input = ogr.Open(shapefile)
+	
+	drv = ogr.GetDriverByName( driverName )  #construct driver object for output file
+	if drv is None:
+		raise RuntimeError("%s driver not available.\n" % driverName)
+	
+	output_shapefile = shapefile+"_filtered"
+	
+	if os.path.exists(output_shapefile):
+		import shutil
+		shutil.rmtree(output_shapefile)
+	
+	output = drv.CreateDataSource(output_shapefile)
+	if output is None:
+		raise ValueError("Creation of output file '%s' failed" % output)
+	
+	# the code below was mostly adapted from the python example at
+	# http://www.gdal.org/ogr/ogr_apitut.html
+	for layer_in in (input.GetLayerByIndex(l) for l in range(input.GetLayerCount())):
+		print(layer_in.GetName())
+		((left, bottom), (right, top)) = elora;
+		layer_in.SetSpatialFilterRect(left, bottom, right, top)  
+		clip = layer_in.GetSpatialFilter().Clone()
+		
+		layer_in.SetSpatialFilter(None)   #unset the filter since we're doing it manually
+		
+				
+		# all good? make the destination Layer
+		layer_out = output.CreateLayer(layer_in.GetName()+"__elora",
+		                               layer_in.GetSpatialRef(),
+		                               layer_in.GetGeomType())
+		
+		if not layer_out:
+			raise RuntimeError("Layer creation failed.")
+		
+		# we need to clone the schema to the new layer
+		# if we don't do this, the code will run and will copy records
+		# but the only data will be the FIDs (which are generated upon adding)
+		schema = layer_in.GetLayerDefn();
+		for field in (schema.GetFieldDefn(i) for i in range(schema.GetFieldCount())):
+			layer_out.CreateField(field)
+		
+		copied = 0
+		for i, feature in enumerate((layer_in.GetFeature(f) for f in range(layer_in.GetFeatureCount()))):
+			#print "Reading FID",feature.GetFID()
+			#g = feature.geometry()
+			#print(g)
+			#print(clip)
+			#IPython.embed()
+			if not clip.Intersects(feature.geometry()):
+			#	print "Skipping FID",feature.GetFID()
+				continue
+			if layer_out.CreateFeature(feature) != 0:
+				raise RuntimeError("Failed copying feature FID%d to shapefile '%s'." % (feature.GetFID(), shapefile_out))
+			copied += 1
+			if i % 10000 == 72:
+				print "Copied", i, "records"
+			
+		print "Copied", copied, "records"
+		
+		assert output.SyncToDisk() == 0, "Failed to save!" #XXX better error needed
+	
+
+
 def filter_to_elora():
+	"attempt to programmatically filter the ARI dataset to only those elements that are relevant to our interests"
+	"this is buggy: gdal doesn't behave like the docs say it should"
 	driverName = "ESRI Shapefile"
 	
 	# near Elora, Ontario, which is
@@ -41,7 +165,7 @@ def filter_to_elora():
 	shapefile = "../assets/maps/OGDE_ARI/AgricultureResourceInventory_1983"
 	input = ogr.Open(shapefile)
 	
-	drv = ogr.GetDriverByName( driverName )
+	drv = ogr.GetDriverByName( driverName )  #construct driver object for output file
 	if drv is None:
 		raise RuntimeError("%s driver not available.\n" % driverName)
 	
@@ -77,7 +201,7 @@ def filter_to_elora():
 		#layer_in.SetSpatialFilterRect(-83, 43, -82, 44)  #this seems to be filtering *out* everything in this layer
 		layer_in.SetSpatialFilterRect(-82, 44, -83, 43)  #flipped??
 		
-		
+		#attempt to debug SetSpatialFilterRect() by printing its output to the map
 		filter_rectangle = layer_in.GetSpatialFilter();
 		#assert layer_out.CreateFeature(filter_rectangle) == 0;
 		print "filter", filter_rectangle.ExportToJson()
@@ -137,4 +261,4 @@ def main():
 
 if __name__ == '__main__':
 	#main()
-	filter_to_elora()
+	filter_to_elora_really()
