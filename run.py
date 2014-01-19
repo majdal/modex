ec06fdc13eeb76f109e5201f7a11b6ddc24bea71
kousu <nick@kousu.ca>
2014-01-19 18:40:58 -0500

    Oops! run.py was leaving a zombie server around!
    Clean up the style at the same time.

--- run.py
+++ run.py
@@ -58,23 +58,31 @@ def which(name, flags=os.X_OK):  #this function is from twisted 8.2's twisted.py
     return result
 
 
-
-import threading
-from subprocess import *
-import webbrowser
+import threading, webbrowser, time
+import subprocess
 
 #'working directory': not the system working directory, but the directory this program is in (so that we can be run from anywhere and$
 PROJECT_ROOT = dirname(abspath(__file__)) #run.py should always sit in the project root
 
-# attempt, in a reasonably cross-platform way, to find the python2 interpreter
-# this depends on twisted which means this script itself depends on python2, which is sort of irritating
-PYTHON = which("python2")
-if not PYTHON:
-  PYTHON = which("python")
-PYTHON = PYTHON[0] #just take the first result as the final result, because whatever
+def browse():
+  time.sleep(2) #give time for the server to spin up
+  webbrowser.open('http://127.0.0.1:8080')  #the URL here is hardcoded and needs to match what server.py spins up on
 
-server = Popen([PYTHON, pathjoin(PROJECT_ROOT, "src", "backend", "server.py"), "debug"])
-browser = threading.Thread(target=lambda: webbrowser.open('http://127.0.0.1:8080')) #the URL here is hardcoded and needs to match what server.py spins up on
-
-browser.start()  #open a browser to the webapp's page (nonblocking)
-server.wait()    #join the server thread              (blocking)
+if __name__ == '__main__':
+  # attempt, in a reasonably cross-platform way, to find the python2 interpreter
+  # this depends on twisted which means this script itself depends on python2, which is sort of irritating
+  #PYTHON = which("python2")
+  #if not PYTHON:
+  #  PYTHON = which("python")
+  #PYTHON = PYTHON[0] #just take the first result as the final result, because whatever
+  PYTHON = 'python2'  #at the moment, 'python2' vs 'python3' seems to be the more cross-compatible method this way
+  
+  browser = threading.Thread(target=browse) 
+  browser.start()  #open a browser to the webapp's page (nonblocking)
+                   # this is glitchy: depending on the mixture of close buttons and ctrl-cs sent
+  
+  # even though the script we're emulating runs the server in the background
+  # it is cleaner to run everything else in the background and run the server on the mainthread
+  # because call() hooks SIGINT (or your local system's favourite shutdown signal)
+  # and politely but firmly kills the server no matter how run.py ends
+  subprocess.call([PYTHON, pathjoin(PROJECT_ROOT, "src", "backend", "server.py"), "debug"])
