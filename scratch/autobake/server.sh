c9ae509daa90f6f14fd76d88bbaca1c898108785
kousu <nick@kousu.ca>
2014-02-12 20:23:01 -0500

    AutobahnPython server to go with wamp.html

---
+++ scratch/autobake/server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#this is the proper invocation for this
+# this is derived from the current latest edition of the samples:
+# https://github.com/tavendo/AutobahnPython/commit/e615b95f41
+
+python2 server.py -d --component backend.Component
