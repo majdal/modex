8f38b4913791b990fd67f2a671fd4fcafb90cd6a
kousu <nick@kousu.ca>
2014-02-21 22:09:54 -0500

    save memory, perhaps

--- scratch/tiling/gen.py
+++ scratch/tiling/gen.py
@@ -5,17 +5,11 @@ import sys, os, os.path
 #using the pillow fork of PIL http://pillow.readthedocs.org/en/latest/reference/ImageDraw.html
 from PIL import Image, ImageDraw
 
-def tile(size, colour):
-	"generate a tile; returns a PIL Image object"
-	I = Image.new('RGB', size)
-	brush = ImageDraw.Draw(I)
-	brush.rectangle([(0,0), I.size], fill=colour)
-	return I
-
 def generate(root):
 	"create a repository of zoom/longitude/latitude (ie z/x/y)"
 	"file structure commonly used (e.g. by OpenStreetMap)"
 	"in folder 'root'"
+
 	
 	# gotcha: the file structure is not totally standardized: the meaning of tiles is.. unclear; the; Bing uses a different scheme
 	# hmmm somehow ol3js knows how to deal with running off the end 
@@ -23,14 +17,21 @@ def generate(root):
 		raise ValueError("'%s' already exists; delete or move it before trying again")
 	os.mkdir(root)
 	os.chdir(root)
+
+
+	T = Image.new('RGB', (256,256)) #256 seems to be the standard size?
+	brush = ImageDraw.Draw(T)
+
 	for z in range(20):
 		os.mkdir(str(z))
 		os.chdir(str(z))
+		print("zoom level", z)
 		for x in range(256):
 			os.mkdir(str(x))
 			os.chdir(str(x))
 			for y in range(256):
-				T = tile((256,256), (x,y,z)) #256 seems to be the standard size?
+				colour = (z,x,y)
+				brush.rectangle([(0,0), T.size], fill=colour)
 				T.save(str(y)+".png")
 			os.chdir("..") #unixism
 		os.chdir("..")
