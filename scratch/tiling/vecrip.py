75aa0f210d704d2df00a8f204c3d1884ae260407
kousu <nick@kousu.ca>
2014-02-22 22:12:37 -0500

    since OSM's servers fake tiles they don't have, just hardcode an extent to rip.
    (actually, OSM autogens tiles on request, with a cache in Varnish)

--- scratch/tiling/vecrip.py
+++ scratch/tiling/vecrip.py
@@ -13,6 +13,7 @@ template = "http://{server}.tile.openstreetmap.us/{archive}/{z}/{x}/{y}.json"
 # OSM's standard is zoom/x/y
 #...why are OSM's vector tiles in z/x/y
 # and their raster tiles in z/y/x? are they?
+# they aren't. that is false. untrue. non.
 
 class DownloadError(Exception): pass
 
@@ -22,40 +23,39 @@ def get(z,x,y):
   url = template.format(server = random.choice(["a","b","c"]),
                               archive = archive,
                               z = z, x = x, y = y)
-  print(url)
+
   tile = requests.get(url)
   if tile.status_code != 200:
     raise InvalidURL("Couldn't download",z,x,y, tile.status_code)
-  print(tile)
+  if not ('features' in tile.json() and tile.json()['features']):
+    #print("\tempty")
+    return #OSM generates a geojson file that looks valid but is empty if you ask for an area it doesn't know about; skip saving these
+  print(url)  
   os.makedirs(os.path.join(str(z),str(x)), exist_ok=True)
   with open(os.path.join(*[str(e) for e in [z,x,y]])+".json","w") as j:
     j.write(tile.text)
 
-def rip(root, depth=20):
+def rip(root, zoom, extent):
   "root is a folder to be created for storage"
-  #"extent is a list [(x0,y0),(xl,yl)] which gives the rectangle of tiles to download"
-  #"extent only makes sense with reference to a particular zoom level..."
+  "extent is a list [(x0,y0),(xl,yl)] which gives the rectangle of tiles to download"
+  "extent only makes sense with reference to a particular zoom level..."
   if os.path.exists(root):
     raise ValueError("'%s' already exists; delete or move it before trying again" % root)
   os.mkdir(root)
   os.chdir(root)
 
-  for z in [15]: #range(depth): #it seems that OSM only has vector tiles at zoom level 15
+  for z in [zoom]: #range(depth): 
     print("zoom level", z)
-    x = 0
-    while True: #we don't know how many tiles exist at each zoom level, so we need to guess and check (ie while + break)
-      y = 0
-      while True: #loop over y
+    for x in range(*extent[0]):
+      for y in range(*extent[1]):
         try:
           get(z,x,y)
         except InvalidURL: #using exceptions as messaging
           print("OpenStreetMap ran out of tiles at y =", y)
           break
-        y += 1
-      if y == 0: #sketchy, but I don't know how else to define "we ran off the right edge of the map" than "asking for the first of the right edge failed"
-        print("OpenStreetMap ran out of tiles at (z,x,y) =",(z,x,y))
-        break
-      x += 1 #hm.. this never knows how to break
+      
+      # how do I communicate "there's no more tiles past this extent"?
 
 if __name__ == '__main__':
-  rip(sys.argv[1])
+  #it seems that OSM only has vector tiles for buildings at zoom level 14 and 15
+  rip(sys.argv[1], zoom=15, extent=[[4500,6000],[4500,6000]])
