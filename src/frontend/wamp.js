56415bf60160360ca5291af05a91893db7c41f4f
kousu <nick@kousu.ca>
2014-02-25 02:24:06 -0500

    use jQuery to denote whether the system is connected yet or not
    also, mostly just to demo this (in 'yellow' state)
    but partially for good fluidity,
    move the code that binds handlers to the WAMP connection
     into a deferred subroutine, using setTimeout()

--- src/frontend/wamp.js
+++ src/frontend/wamp.js
@@ -5,8 +5,10 @@
     console.log("Call failed: " + JSON.stringify(error));
   }
 
-
-  $(function() {
+  
+$(function() {
+ $("#connected").css("color", "red") //mark not connected
+ $("#connected").html("Disconnected") 
  
  var connection = new autobahn.Connection({
    url: 'ws://127.0.0.1:8080/wamp',
@@ -15,6 +17,8 @@
  global.connection = connection; //debug
 
   connection.onopen = function (session) {
+    $("#connected").css("color", "yellow") //mark connecting
+    $("#connected").html("Connecting..")
     console.log("opening session " + session.id);
     console.log(connection._websocket.readyState);
 
@@ -29,7 +33,8 @@
   connection._websocket.onclose = function(evt) { 
     console.log("websocket close: " + evt.reason)
   }
-
+  
+  function hookup() {
   session.subscribe("heartbeat", function() {
     console.log("heartbeat");
   })
@@ -48,10 +53,25 @@
       console.log("this is not a parrot nevermore:", JSON.stringify(r));
     }
     //session.call('start').then(parrotHandler);
-  
+    
+    $("#connected").css("color", "green") //mark connected
+    $("#connected").html("Connected.")
+
     global.session = session; //stash for debugging
+    
+    start = $("#controls #start") //ack; my spider sense wants this to be 
+    stop = $("#controls #stop")
+    start.css("opacity", 1) //mark ready to start (XXX not sync'd with server side _running/not _runningx state)
+    //start.click(function() {
+    //  start.opacity("
+    //})
+    global.start = start;
+    global.stop = stop;
+   }
+  setTimeout(hookup, 6000)
   }
 
-  connection.open()
+ setTimeout(function() {
+  connection.open()}, 6000)
 
 }) //end oninit
