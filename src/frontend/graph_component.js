999a28b43cab87db935a5e04f1e7c8cf978c618c
kousu <nick@kousu.ca>
2014-02-25 00:28:08 -0500

    HTML cleanup; this commit breaks things, but I'll fix it in the next few.

--- src/frontend/graph_component.js
+++ src/frontend/graph_component.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
         .y(function(d) { return y(d.temperature); });
 
     var svg = d3.select("#graph").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        //.attr("width", width + margin.left + margin.right)
+        //.attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
