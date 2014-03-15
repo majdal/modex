884798bf828d3112dd3121ac84267c77c0f12da6
kousu <nick@kousu.ca>
2014-03-15 04:44:21 -0400

    Excise WAMP
    It can come back
    If we really need it
    Which we won't
    
    103 lines is sooo not worth it.

--- src/backend/server.py
+++ src/backend/server.py
@@ -13,8 +13,6 @@ from twisted.python import log
 from twisted.web.server import Site
 from twisted.web.static import File
 
-import autobahn.twisted.wamp
-
 # autobahn changed their API between (which happens to be within the last month of so as of this commit)
 # they added asyncio (which is py3.4-native) as an alternate option to twisted (which is py2-native),
 # and hence moved moved classes from autobahn.* to autobahn.twisted.*
@@ -27,20 +25,10 @@ import autobahn.twisted.wamp
 #  https://raw.github.com/tavendo/AutobahnPython/e1dae070e67a9361f14beba775c66961e06d43ff/demo/echo/echo_server.py
 
 from autobahn.twisted.websocket import WebSocketServerFactory, WampWebSocketServerFactory, WebSocketServerProtocol, WampWebSocketServerProtocol
-
-#from autobahn.wamp.websocket import WampWebSocketServerProtocol, WampWebSocketServerFactory
 from autobahn.twisted.resource import * #WebSocketResource, HTTPChannelHixie76Aware
 
-#TODO: import gdal and take vector layers to load as arguments
-#TODO: Autobahn as of 0.7.4 actually supports py3, even though Twisted doesn't; this might be worth investigating.
 
 import json
-
-#data = {'Incandescent': [-_ for _ in range(10)],
-#                'CFL': [_ for _ in range(10)],
-#                'Halogen': [_*2 for _ in range(10)],
-#                'LED': [_*0.5 for _ in range(10)],
-#        }
 
 # we are trying to set up a producer-consumer system, and twisted has this built in:
 # https://twistedmatrix.com/documents/12.2.0/core/howto/producers.html
@@ -49,65 +37,13 @@ import json
 # TODO(kousu): set up WAMP and use it to push messages instead of using a 'raw' websocket
 
 
-import random #XXX this has been moved to models/beta.py, but is copied here because instead of writing a model-loading structure
-ALPHA = .5
-BETA = 10
-class Model(object):
-    """
-    a stub 'model' that gives values from a beta distribu
-    this model is an iterator and is time-stepped by calling next() on it
-    """
-    def __init__(self, alpha, beta):
-        self._alpha = alpha
-        self._beta = beta
-    def __iter__(self): return self
-    def next(self):
-        return random.betavariate(self._alpha, self._beta)
-
-
 import csv
 
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.util import sleep
-from autobahn.twisted.wamp import ApplicationSession, ApplicationSessionFactory
 
 
 
-class ModelServer(ApplicationSession):
-   """
-   An application component that publishes events with no payload
-   and with complex payloads every second.
-   """
-   def __init__(self):
-      ApplicationSession.__init__(self)
-      self._model = Model(ALPHA, BETA) #NB must be before join since .join() causes onJoin()... though.. uh.. @inlineCallbacks is weird
-      self._running = False #NB: models don't know about real time, but ModelServer does
-
-   def onConnect(self):
-      self.join("realm1") # only one "realm" can be joined at a time
-   
-   def start(self):
-      self._running = True
-   
-   def stop(self):
-      self._running = False
-   
-   @inlineCallbacks
-   def onJoin(self, details):
-
-      self.register(self.start, "start")
-      self.register(self.stop, "stop")
-      
-      counter = 0
-      while True:
-         self.publish('heartbeat')
-         
-         if self._running:
-           state = next(self._model) #NB: model is an iterator
-           self.publish('data', state) # ... this seems.. poor.. in the long run. The models internally need to be able to register data streams on themselves,
-                                       # and then we need 
-
-         yield sleep(2)
 
     
 #TODO(kousu): move this out to scratch/ for reference on how to host a web socket server using AutobahnPython
@@ -183,17 +119,6 @@ class JsonDataServer(WebSocketServerProtocol):
      self.sendMessage(json.dumps(o), False)
       
 
-   ## create a WAMP router factory
-   ##
-from autobahn.wamp.router import RouterFactory
-from autobahn.twisted.wamp import RouterSessionFactory
-
-   
-     
-## create a WAMP router session factory
-##
-
-
 if __name__ == '__main__':
 
    if len(sys.argv) > 1 and sys.argv[1] == 'debug':
@@ -215,33 +140,12 @@ if __name__ == '__main__':
                                     debug = debug,
                                     debugCodePaths = True)
    ctl_endpoint.protocol = CtlProtocol
-   
-
-   from autobahn.wamp.router import RouterFactory
-   router_factory = RouterFactory()
-
-     
-   ## create a WAMP router session factory
-   ##
-   from autobahn.twisted.wamp import RouterSessionFactory
-   session_factory = RouterSessionFactory(router_factory)
-   
-   ## [ ... ... ]
-   session_factory.add(ModelServer())
-   
-   ## create a WAMP-over-WebSocket transport server factory
-   ##
-   from autobahn.twisted.websocket import WampWebSocketServerFactory
-   wamp_factory = WampWebSocketServerFactory(session_factory, "ws://localhost:8080", debug = True)
-   wamp_factory.setProtocolOptions(failByDrop = False)
-   
-   
+      
    webroot = pathjoin(PROJECT_ROOT,"src","frontend")
    assets = pathjoin(PROJECT_ROOT,"assets")
    if debug:
      print "putting", webroot,"at root"
      print "putting", assets,"at assets"
-   
    
    ## we serve static files (most of the frontend html, js, and css) under "/" ..
    ## except for some which are under assets/
@@ -250,21 +154,18 @@ if __name__ == '__main__':
    assets = File(assets)
    data_resource = WebSocketResource(data_endpoint)
    ctl_resource = WebSocketResource(ctl_endpoint)
-   wamp = WebSocketResource(wamp_factory)
    
    root.putChild("assets", assets)  #TODO: do we prefer to have each entry in assets/ sit at the root (ie http://simulation.tld/data/ instead of http://simulation.tld/assets/data/)   
    root.putChild("ws", data_resource)    #this puts the websocket at /ws. You cannot put both the site and the websocket at the same endpoint; whichever comes last wins, in Twisted
    if debug:
      root.putChild("scratch", File(pathjoin(PROJECT_ROOT,"scratch")))
-   #root.putChild("wamp", wamp) #okay, this is not behaving itself; crud
    root.putChild("ctl", ctl_resource) #this whole file is so not pythonic. Where's the D.R.Y. at, yo? --kousu
    
    #. <- /
    #./scratch <- /scratch
    #websocket <- /ws #random demo socket
    #websocket <- /ctl
-   #websocket <- /wamp 
-   #RESTful 
+   #RESTful ??
    
    ## both under one Twisted Web Site
    site = Site(root)
