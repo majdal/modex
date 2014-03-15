3f7dac97aa7aa254e7abd5b5d0f71cd74250a4a9
Nathan Wilson <legobuster@hotmail.com>
2014-03-14 22:47:41 -0400

    Changed graph to use nvd3

--- src/frontend/graph.js
+++ src/frontend/graph.js
@@ -1,3 +1,5 @@
+
+
 $(function() {
 /*
  *   TODO
@@ -14,6 +16,8 @@ $(function() {
 
    scope = {} //hacks
    scope.data = [] //makes a data array that is safely accessible from anywhere in this file; sidesteps any weird js scoping rules that might kick in if we tried to make data a (pseudo)global
+   
+   var parseDate = d3.time.format("%Y%m%d").parse;
     
     var margin = {top: 20, right: 80, bottom: 30, left: 50},
     
@@ -21,30 +25,42 @@ $(function() {
     //height = 500 - margin.top - margin.bottom;
     width = $("#graph").width() - margin.left - margin.right
     height = $("#graph").height() - margin.top - margin.bottom;
+        
+	var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"
+        );
+	var chart;
+	
+	nv.addGraph(function() {
+		chart = nv.models.lineChart()
+		.options({
+			margin: {top: 20, right: 350, bottom: 30, left: 50},
+			showXAxis: true,
+			showYAxis: true,
+			showLegend: true,
+		})
+		;
+		
+		chart.xAxis
+			.axisLabel("Time")
+			.tickFormat(function(d) { return d3.time.format('%b %d, %Y')(new Date(d)) })
+			;
+		chart.yAxis
+			.axisLabel("Temperature ((ºF)")
+			.tickFormat(d3.format(',.1f'))
+			;
+			
+		chart.x(function(d,i) { return d.x}); 	// use the .x value for x-axis 
+                                           		//  location (rather than i)
+                     
+        
+        nv.utils.windowResize(chart.update);
 
-    var parseDate = d3.time.format("%Y%m%d").parse;
-
-    var x = d3.time.scale()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var color = d3.scale.category10();
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.temperature); });
-
+  		return chart;
+	})
     
     
     
@@ -53,98 +69,59 @@ $(function() {
        this.send(""); //poke the server to get data out
     }
     data_socket.onmessage = function(d) {
+      
       d = JSON.parse(d.data);
       //console.log("received data from (plain) websocket:")
-      d.date = parseDate(d.date)
+      d.date = parseDate(d.date);
+      
+      //loop through and format data into line graph data for nv
+      for (key in d){
+      	//date is always used as x value
+      	if( key != "date"){
+      		addData(key, d.date, d[key]);
+      	}
+      }
+      
+      
+      svg // update the chart with the data
+        .datum(scope.data)
+        .call(chart);
       //console.log(d);
-      scope.data.push(d)
-      draw()
+      //scope.data.push(d)
+      //draw()
     }
     
-    /*
-    d3.tsv("assets/data/static_lightbulbs.tsv", function(error, data) {
+    //add Data to pre-existing line
+    function addData(keyval,xval,yval){
+    	var notthere = true
+    	//if there is already a line for the data add it to that line
+    	for(var i = 0; i < scope.data.length; i++){
+    		if(scope.data[i].key == keyval){
+    			notthere = false;
+    			scope.data[i].values.push({x:xval, y:yval});
+    		}
+    	}
+    	
+    	//otherwise add a new line
+    	if(notthere){
+    		var new_line = {
+    		values: [{x:xval, y:yval}],
+    		key: keyval,
+    		color: getRandomColor()
+    		}
+    		scope.data.push(new_line);
+    	}
+    		
+    }
     
-      data.forEach(function(d) {
-        d.date = parseDate(d.date);
-      });
-      
-      scope.data = data;
-      
-      draw()
-      })
-      */
-      
-   // the trickiest part of this problem is that it is several line graphs, not just ones
+    // Just to be pretty
+	function getRandomColor() {
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++ ) {
+			color += letters[Math.round(Math.random() * 15)];
+		}
+		return color;
+	}
    
-   function draw() {
-    data = scope.data; //hacks
-      
-    //console.log("plotting this data array:", data)
-      
-    //console.log("creating svg")
-    $("svg").detach() //kill old svg, if there is one
-    var svg = d3.select("#graph").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"
-        );
-      
-      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-
-      var cities = color.domain().map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {date: d.date, temperature: +d[name]};
-          })
-        };
-      });
-
-      x.domain(d3.extent(data, function(d) { return d.date; }));
-
-      y.domain([ //compute the outermost values we need on the axes to show all the data
-        d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-        d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-      ]);
-
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Temperature (ºF)"); //tempurature?
-
-      //'city' is named because this code was stolen from a demo showing
-      // off using d3 to plot multiple city temperature lines
-      // 'city' is a <g> tag containing a <path>: the line, and
-      //   a <text>: the label at the end of the line
-      var city = svg.selectAll(".city")
-          .data(cities)
-        .enter().append("g")
-          .attr("class", "city");
-
-     // this is <path> tag
-     // 
-      city.append("path")
-          .attr("class", "line")
-          .attr("d", function(d) { return line(d.values); /*this call returns the actual line in the format desired by svg <path> elements */ })
-          .style("stroke", function(d) { return color(d.name); });
-
-      // this is the <text> tag which labels the end of the line
-      city.append("text")
-          .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-          .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-          .attr("x", 3)
-          .attr("dy", ".35em")
-          .text(function(d) { return d.name; });
-   }
-});
+  })
