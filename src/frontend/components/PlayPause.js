71beb6cdb90968fe8ad75be585dbe71201acfebc
Majd Al-shihabi <majd.al@gmail.com>
2014-03-15 14:59:11 -0400

    Fixed the play pause button

--- src/frontend/components/PlayPause.js
+++ src/frontend/components/PlayPause.js
@@ -26,19 +26,19 @@ Crafty.c('PlayPause', {
 	isPaused: true, 
 
 	init : function(){
-		this.requires("2D, Canvas, Mouse, SpriteAnimation, pauseButton");
+		this.requires("2D, Canvas, Mouse, playButton");
 		var positionY = Crafty.viewport.height-250;
 		
 		this.attr({x: 20, y: positionY, w: 100, h: 100})
-		.animate("press", [[20, 20]]).animate("release", [[0, 20]])
+		//.animate("press", [[20, 20]]).animate("release", [[0, 20]])
 		 .bind("Click", function(){
 			if (this.isPaused) {
 				this.addComponent("pauseButton").removeComponent("playButton");
-				this.isPaused = true;
+				this.isPaused = false;
 			}
 			else {
 				this.addComponent("playButton").removeComponent("pauseButton");
-				this.isPaused = false;
+				this.isPaused = true;
 			}
 		}) 
 		
