3f066f9c23f9fc36ff2045e3b2db3c3d0c5af748
feyxo <feyisola.agbaje@gmail.com>
2014-03-14 21:18:24 -0400

    Play/Pause button

--- src/frontend/components/PlayPause.js
+++ src/frontend/components/PlayPause.js
@@ -5,6 +5,8 @@
 * A simple play/pause button for the game
 *
 */
+
+/*
 Crafty.c("PlayPause", {
 	init: function(entity) {
 		this.requires("2D");
@@ -17,5 +19,48 @@ Crafty.c("PlayPause", {
 
 	pause: function() {
 		console.log('pause');
+	},
+});*/
+
+Crafty.c('PlayPause', {
+	isPaused: true, 
+
+	init : function(){
+		this.requires("2D, Canvas, Mouse, SpriteAnimation, pauseButton");
+		var positionY = Crafty.viewport.height-250;
+		var button = {
+			PAUSE : 0,
+		}
+		this.attr({x: 20, y: positionY, w: 100, h: 100})
+		/* .bind("Click", function(){
+			if (this.isPaused) {
+				this.addComponent("pauseButton").removeComponent("playButton");
+				this.isPaused = true;
+			}
+			else {
+				this.addComponent("playButton").removeComponent("pauseButton");
+				this.isPaused = false;
+			}
+		}) 
+	*/	
+		.animate("press", [[20, 20]]).animate("release", [[0, 20]])
+		.bind('MouseDown', function(){
+        buttonAnimate(button.PAUSE, "press");
+    	})
+        .bind('MouseOut', function() { 
+        buttonAnimate(button.PAUSE, "release");
+    	})
+
+    	var buttonAnimate = function(btn, action){
+    		var pauseplay
+
+    		switch (btn){
+    			case button.PAUSE:
+    			pauseplay = pauseButton;
+    			break;
+    		}
+
+    		pauseplay.animate(action, 1, 0);
+    	}
 	},
 });
