67891736d20064a7af4ed13ff9f86b6faf0188ec
Majd Al-shihabi <majd.al@gmail.com>
2014-03-16 15:53:58 -0400

    Clean up run.py

--- run.py
+++ run.py
@@ -6,58 +6,13 @@
 ```
 """
 
-# TODO: this script is untested on Windows
 # TODO: silence messages from the webrowser (they are very distracting)
 
 import sys
 from os.path import dirname, abspath, join as pathjoin
 
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
-# See LICENSE for details.
-
-"""
-Utilities for dealing with processes.
-"""
 
 import os
-
-def which(name, flags=os.X_OK):  #this function is from twisted 8.2's twisted.python.procutils, imported so that this script doesn't depend on twisted itself
-    """Search PATH for executable files with the given name.
-    
-    On newer versions of MS-Windows, the PATHEXT environment variable will be
-    set to the list of file extensions for files considered executable. This
-    will normally include things like ".EXE". This fuction will also find files
-    with the given name ending with any of these extensions.
-
-    On MS-Windows the only flag that has any meaning is os.F_OK. Any other
-    flags will be ignored.
-    
-    @type name: C{str}
-    @param name: The name for which to search.
-    
-    @type flags: C{int}
-    @param flags: Arguments to L{os.access}.
-    
-    @rtype: C{list}
-    @param: A list of the full paths to files found, in the
-    order in which they were found.
-    """
-    result = []
-    exts = filter(None, os.environ.get('PATHEXT', '').split(os.pathsep))
-    path = os.environ.get('PATH', None)
-    if path is None:
-        return []
-    for p in os.environ.get('PATH', '').split(os.pathsep):
-        p = os.path.join(p, name)
-        if os.access(p, flags):
-            result.append(p)
-        for e in exts:
-            pext = p + e
-            if os.access(pext, flags):
-                result.append(pext)
-    return result
-
-
 import threading, webbrowser, time
 import subprocess
 
@@ -75,12 +30,6 @@ if __name__ == '__main__':
   else:
       debug = ''
    
-  # attempt, in a reasonably cross-platform way, to find the python2 interpreter
-  # this depends on twisted which means this script itself depends on python2, which is sort of irritating
-  #PYTHON = which("python2")
-  #if not PYTHON:
-  #  PYTHON = which("python")
-  #PYTHON = PYTHON[0] #just take the first result as the final result, because whatever
   PYTHON = sys.executable # simply use the same python that called the run.py script. 
   
   browser = threading.Thread(target=browse) 
