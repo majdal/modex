5fb815a05c98f317bc076a3de8358d6ef49ed107
kousu <nguenthe@uwaterloo.ca>
2014-05-23 21:22:36 -0400

    Checkpoint: first functional table diffing algorithm.
    Slow, verbose, and the API isn't full there, and it only detects
    changes as adds or deletes, not updates, but it runs.

--- scratch/sqldiff/diff.py
+++ scratch/sqldiff/diff.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # diff.py 
 """
 sqldiff algorithm
@@ -5,6 +6,7 @@ sqldiff algorithm
 
 inputs: two tables with the same columns, fully sorted
 {nb: fully sorted means first sorted on the first column, then by the second to break ties, then by the third.. all the way down to the end; sql's ORDER BY clause performs this for us}
+(other nb: the particular sort uses doesn't matter, so long as it is consistent from run to run)
 
 Call the first table L and the second N
 
@@ -19,54 +21,86 @@ output: whatever remains in L is the "deletes" list and what is in N is "adds"
 
 """
 
+from itertools import *
+
 def negativeidx(idx):
     return [-i for i in idx]
 
 def fancyslice(L, idx): #implements indexing-by-set like R and scipy
-    return [L[i] for i in idx]
+    # TODO: support negative indexing to mean "drop it"
+    return [l for i, l in enumerate(L) if i in idx]
 
-def diff(L, N):
-    L, N = iter(L), iter(N)
+def diff(L, R):
+    assert L == sorted(L)
+    assert R == sorted(R)
+    len_L, len_R = len(L), len(R)
+    L, R = iter(L), iter(R)
     Kept_L, Kept_R = [], []
     i_L, i_R = 0, 0
     # start walking the lists
     # this algorithm is not supposed to be, youy know, elegant. i'll get to that.
     L_row = next(L)
     R_row = next(R)
-    i = 0
-    while True:
-        print()
-        print(i)
-        print("L[%d] = %s" % (i_L, L_row))
-        print("R[%d] = %s" % (i_R, R_row))
-        # ending conditions are complicated 
-        if L_row == R_row:
-            # if terms are equal, then erase them both by stepping the index
-            print("samesies!")
-            L_row = next(L)
-            R_row = next(R)
-            i_L += 1
-            i_R += 1
-        else:
-            # however if they are different, we need to figure out how different
-            print("differences!")
-            # we.. step the left if ..
-            if ....:
-                print("stepping left")
+    i = -1
+    try:
+        while True:
+            i+=1
+            print()
+            print(i)
+            print("L[%d] = %s" % (i_L, L_row))
+            print("R[%d] = %s" % (i_R, R_row))
+            # ending conditions are complicated 
+            if L_row == R_row:
+                # if terms are equal, then erase them both by stepping the index
+                print("samesies!")
+                L_row = next(L)
+                R_row = next(R)
                 i_L += 1
-                L_row= next(L)
-            else:
-                print("stepping right")
                 i_R += 1
-                R_row= next(R)
-            #and the right otherwise..
-            
-    return fancyslice(L, negativeidx(Erased_L)), fancyslice(R, negativeidx(Kept_R) 
+            else:
+                # however if they are different, we need to figure out how different
+                print("differences!")
+                # we.. step the left if ..
+                # TODO: make the common case be 'updated', and 'deleted' be the degenerate update case
+                #
+                if L_row < R_row:
+                    print("stepping left")
+                    Kept_L += [(i_L, L_row)]
+                    i_L += 1
+                    L_row= next(L)
+                else:
+                    #and the right otherwise..
+                    print("stepping right")
+                    Kept_R += [(i_R, R_row)]
+                    i_R += 1
+                    R_row= next(R)
+    except StopIteration:
+        # finish up any leftovers
+        Kept_L += islice(L, i_L, len_L)
+        Kept_R += islice(R, i_R, len_R)
+
+    print(Kept_L)
+    print(Kept_R)        
+    #return fancyslice(L, Kept_L), fancyslice(R, Kept_R)
+    return Kept_L, Kept_R
+
+def read_table(fname):
+    """
+    load a csv , and sort it to serve the preconditions
+    drops the header row!
+    Inefficient!
+    """
+    return sorted(list(csv.reader(open(fname,"r")))[1:])
+
 
 if __name__ == '__main__':
     import sys
     import csv
-    L, R = [csv.reader(open(f)) for f in sys.argv[1:]]
+    L, R = [read_table(f) for f in sys.argv[1:]]
     deletions, additions = diff(L, R)
-    print(additions)
-    print(deletes)
+    print("additions")
+    for a in additions:
+        print(a)
+    print("deletions")
+    for d in deletions:
+        print(d)
